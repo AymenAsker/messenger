05246ba169f66b5703023026ecec01899b90b9a0
AymenAsker <aymenya1997@gmail.com>
2022-11-13 21:56:10 +0200

    add

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         react(),
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.jsx',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+            },
+        },
+    },
 });
